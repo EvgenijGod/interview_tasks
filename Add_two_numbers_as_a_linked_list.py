b49b86627880677ca17d9548c6221a4d066e3d08
EvgenijGod <55794716+EvgenijGod@users.noreply.github.com>
2020-05-26 20:54:34 +0300

    Update Add_two_numbers_as_a_linked_list.py

--- Add_two_numbers_as_a_linked_list.py
+++ Add_two_numbers_as_a_linked_list.py
@@ -1,3 +1,10 @@
+"""
+You are given two linked-lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
+"""
+
+
 class ListNode(object):
     def __init__(self, x):
         self.val = x
